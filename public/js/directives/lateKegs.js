895f7625b4d9807d4dbba2b877c5813be143291b
Colm O'Shea <colmoshea@mcqs.com>
2016-06-01 13:29:24 +0100

    Fixed late kegs table collapse boolean and added dummy data

--- public/js/directives/lateKegs.js
+++ public/js/directives/lateKegs.js
@@ -9,7 +9,29 @@ app.directive('lateKegsDirective', function(){
         },
         link: function (scope) {
             scope.showKegs = false;
-            scope.displayedKegs = [];
+            scope.displayedKegs = [
+                {
+                    kegNo: 1,
+                    size: 50,
+                    dest: 'Tralee',
+                    dateEntered:  new Date("01/06/16 23:00:00 UTC"),
+                    daysLate: 3
+                },
+                {
+                    kegNo: 1,
+                    size: 50,
+                    dest: 'Tralee',
+                    dateEntered:  new Date("01/03/16 23:00:00 UTC"),
+                    daysLate: 60
+                },
+                {
+                    kegNo: 1,
+                    size: 50,
+                    dest: 'Dingle',
+                    dateEntered:  new Date("03/05/16 23:00:00 UTC"),
+                    daysLate: 33
+                }
+            ];
         }
     }
 });
